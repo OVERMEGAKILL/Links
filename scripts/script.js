1397eb182f3fc62eb2f2264e7a321c7d34b94012
OVERMEGAKILL <overmegakill@proton.me>
2024-09-05 12:38:34 +0200

    Update script.js
    new script

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,15 @@
-﻿document.addEventListener('mousemove', (e) => {
+document.addEventListener('mousemove', (e) => {
     const container = document.querySelector('.container');
     const x = (window.innerWidth / 2 - e.pageX) / 20;
     const y = (window.innerHeight / 2 - e.pageY) / 20;
     container.style.transform = `rotateX(${y}deg) rotateY(${x}deg)`;
 });
 
-function typeEffect(element, text, delay = 100) {
-    let i = 0;
-    element.innerHTML = '';
-    const interval = setInterval(() => {
-        if (i < text.length) {
-            element.innerHTML += text.charAt(i);
-            i++;
-        } else {
-            clearInterval(interval);
-        }
-    }, delay);
-}
-
-function translateEffect(element, texts, delay = 100) {
+function changeFont(element, fonts, delay = 500) {
     let i = 0;
     const interval = setInterval(() => {
-        if (i < texts.length) {
-            element.innerHTML = texts[i];
+        if (i < fonts.length) {
+            element.style.fontFamily = fonts[i];
             i++;
         } else {
             clearInterval(interval);
@@ -32,18 +19,10 @@ function translateEffect(element, texts, delay = 100) {
 
 function animateLinks() {
     const links = document.querySelectorAll('.link');
-    const terminal = document.querySelector('.terminal');
-    terminal.style.display = 'block';
     links.forEach((link, index) => {
         setTimeout(() => {
-            const alienText = '⟟⟊⟟⟊⟟⟊';
-            const cyrillicText = 'Пример';
-            const japaneseText = '例';
-            const latinText = link.getAttribute('data-text');
-            typeEffect(terminal, alienText, 100);
-            setTimeout(() => {
-                translateEffect(terminal, [alienText, cyrillicText, japaneseText, latinText], 500);
-            }, 1000);
+            const fonts = ['LovecraftsDiary', 'Beograd', 'LAKOSHEN', 'SAIBA'];
+            changeFont(link, fonts, 500);
         }, index * 5000);
     });
 }
