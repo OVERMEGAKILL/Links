5f654a14ef3b8e2da1485543623a6165f55e8f06
OVERMEGAKILL <overmegakill@proton.me>
2024-09-10 15:48:26 +0200

    Update script.js
    spline script js manually merged

--- scripts/script.js
+++ scripts/script.js
@@ -5,14 +5,52 @@ document.addEventListener('mousemove', (e) => {
     container.style.transform = `rotateX(${y}deg) rotateY(${x}deg)`;
 });
 
-function changeFont(element, fonts, delay = 500) {
+function typeEffect(element, text, delay = 100) {
     let i = 0;
+    element.innerHTML = '';
     const interval = setInterval(() => {
-        if (i < fonts.length) {
-            element.style.fontFamily = fonts[i];
+        if (i < text.length) {
+            element.innerHTML += text.charAt(i);
             i++;
         } else {
             clearInterval(interval);
+        }
+    }, delay);
+}
+
+function changeFontLetterByLetter(element, fonts, delay = 500) {
+    const text = element.getAttribute('data-text');
+    let i = 0;
+    const interval = setInterval(() => {
+        if (i < text.length) {
+            const span = document.createElement('span');
+            span.textContent = text.charAt(i);
+            span.style.fontFamily = fonts[0];
+            element.appendChild(span);
+            i++;
+        } else {
+            clearInterval(interval);
+            setTimeout(() => {
+                translateFonts(element, fonts.slice(1), delay);
+            }, 1000);
+        }
+    }, delay);
+}
+
+function translateFonts(element, fonts, delay) {
+    const spans = element.querySelectorAll('span');
+    let i = 0;
+    const interval = setInterval(() => {
+        if (i < spans.length) {
+            spans[i].style.fontFamily = fonts[0];
+            i++;
+        } else {
+            clearInterval(interval);
+            if (fonts.length > 1) {
+                setTimeout(() => {
+                    translateFonts(element, fonts.slice(1), delay);
+                }, 1000);
+            }
         }
     }, delay);
 }
@@ -21,10 +59,14 @@ function animateLinks() {
     const links = document.querySelectorAll('.link');
     links.forEach((link, index) => {
         setTimeout(() => {
-            const fonts = ['LovecraftsDiary', 'Beograd', 'LAKOSHEN', 'SAIBA'];
-            changeFont(link, fonts, 500);
+            link.innerHTML = '';
+            typeEffect(link, link.getAttribute('data-text'), 100);
+            setTimeout(() => {
+                const fonts = ['LovecraftsDiary', 'Beograd', 'LAKOSHEN', 'SAIBA'];
+                changeFontLetterByLetter(link, fonts, 500);
+            }, 1000);
         }, index * 5000);
     });
 }
 
-setInterval(animateLinks, 15000);
+setInterval(animateLinks, 22000);
